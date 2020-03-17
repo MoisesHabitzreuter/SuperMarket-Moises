267b58632887c93989a267f385b4f5bf1c46a27e
joao victor fritzke <joaovictorfritzke2003@hotmail.com>
2020-03-17 14:53:35 -0300

    comiit and

---
+++ DTO/CartDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class CartDTO
+    {
+        public ICollection<ProductDTO> Products { get; set; }
+    }
+}
