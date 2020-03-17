fc2f2f66c67f4fdb1c2d69b449ae7aaa2be6b98b
Moises Henrique Habitzreuter <moises.habitzreuter@hbsis.com.br>
2020-03-17 14:45:36 -0300

    Teste

--- DAL/Impl/CategoryRepositoryADO.cs
+++ DAL/Impl/CategoryRepositoryADO.cs
@@ -18,58 +18,7 @@ namespace DAL.Impl
         }
         public Task<List<CategoryDTO>> GetCategories()
         {
-            //Objeto que se conecta a bases SQLSERVER
-            SqlConnection connection = new SqlConnection();
-
-            //Vincula a connection string ao objeto que gerencia conexões 
-            connection.ConnectionString = _options.ConnectionString;
-
-            //Objeto que realiza comandos em bases SQLSERVR
-            SqlCommand command = new SqlCommand();
-
-            command.CommandText = "SELECT * FROM CATEGORIES";
-            command.Connection = connection;
-
-            try
-            {
-                connection.Open();
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                List<CategoryDTO> categories = new List<CategoryDTO>();
-
-                //Enquanto houver registros, leia!
-                while (reader.Read())
-                {
-                    //Exemplo utilizando um cast, veloz, porém perigoso
-                    //em caso de migração de base
-                    //string nome = (string)reader["NAME"];
-                    //Criando um gênero pra representar o registro no banco
-                    CategoryDTO c = new CategoryDTO(Convert.ToInt32(reader["ID"]),
-                                       (string)reader["NOME"],
-
-
-                    //Adicionando o gênero na lista criada acima.
-                    categories.Add(c);
-                }
-                DataResponse<Cliente> response = new DataResponse<Cliente>();
-                response.Sucesso = true;
-                response.Data = clientes;
-                return response;
-            }
-            catch (Exception ex)
-            {
-                //Logar o erro pro adm do sistema ter acesso.
-                File.WriteAllText("log.txt", ex.Message);
-                DataResponse<Cliente> response = new DataResponse<Cliente>();
-                response.Sucesso = false;
-                response.Erros.Add("Falha ao acessar o banco de dados, contate o suporte.");
-                return response;
-            }
-            finally
-            {
-                connection.Dispose();
-            }
+            throw new NotImplementedException();
         }
 
         public async Task Insert(CategoryDTO category)
--- DTO/CategoryDTO.cs
+++ DTO/CategoryDTO.cs
@@ -7,17 +7,6 @@ namespace DTO
 {
     public class CategoryDTO
     {
-        public CategoryDTO()
-        {
-        }
-
-        public CategoryDTO(int iD, string name, ICollection<ProductCategoryDTO> productCategory)
-        {
-            ID = iD;
-            Name = name;
-            ProductCategory = productCategory;
-        }
-
         public int ID { get; set; }
         public string Name { get; set; }
         public virtual ICollection<ProductCategoryDTO> ProductCategory { get; set; }
